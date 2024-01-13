5fe1d5cefdc82b549abd9fc05a75fa01a3c5fa22
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2024-01-13 00:06:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask, request
-
-from scraper import get_scrap_completion
+from random import choice
+import g4f
 
 flask_app = Flask(__name__)
+
+providers = {
+    'gpt-3.5-turbo': [
+        g4f.Provider.ChatBase,
+        g4f.Provider.Bing,
+        g4f.Provider.ChatgptAi,
+        g4f.Provider.FakeGpt,
+        g4f.Provider.FreeGpt,
+        g4f.Provider.GPTalk,
+        g4f.Provider.GptForLove,
+        g4f.Provider.GptGo,
+        g4f.Provider.Hashnode,
+        g4f.Provider.You,
+    ],
+    'gpt-4': [
+        g4f.Provider.Bing
+    ]
+}
+
+allowed_models = [
+    'gpt-3.5-turbo',
+    'gpt-4'
+]
+
+
+async def get_scrap_completion(model: str, messages: list) -> str or None:
+    if not model in allowed_models:
+        return None
+
+    response = await g4f.ChatCompletion.create_async(
+        model=model,
+        messages=messages,
+        provider=choice(providers[model]),
+    )
+    return response
 
 
 @flask_app.post('/v1/chat/completions/')
