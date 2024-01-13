2e9d8fda37e82bea8e3bee8dbe0f188064f72a7c
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2024-01-12 23:33:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from typing import Union
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from asgiref.wsgi import WsgiToAsgi
+from flask import Flask, request
 
 from scraper import get_scrap_completion
 
-
-app = FastAPI(debug=True)
-
-
-class Petition(BaseModel):
-    model: str
-    messages: list | None = None
+flask_app = Flask(__name__)
 
 
-@app.post('/v1/chat/completions/')
-async def get_completion(petition: Petition):
-    response = await get_scrap_completion(petition.model, petition.messages)
+@flask_app.post('/v1/chat/completions/')
+async def get_completion():
+    response = await get_scrap_completion(request.json['model'], request.json['messages'])
     if response is None:
         return {'error': 'no completion available'}
     return {
@@ -29,3 +22,8 @@ async def get_completion(petition: Petition):
             }
         ]
     }
+
+
+app = WsgiToAsgi(flask_app)
+
+#app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,24 +1,39 @@
 from random import choice
 import g4f
 
-providers = [
-    g4f.Provider.Bing,
-    #g4f.Provider.GeekGpt,
-    g4f.Provider.GptChatly,
-    g4f.Provider.Liaobots,
-]
+
+providers = {
+    'gpt-3.5-turbo': [
+        g4f.Provider.ChatBase,
+        g4f.Provider.Bing,
+        g4f.Provider.ChatgptAi,
+        g4f.Provider.FakeGpt,
+        g4f.Provider.FreeGpt,
+        g4f.Provider.GPTalk,
+        g4f.Provider.GptForLove,
+        g4f.Provider.GptGo,
+        g4f.Provider.Hashnode,
+        g4f.Provider.You,
+    ],
+    'gpt-4': [
+        g4f.Provider.Bing
+    ]
+}
 
 allowed_models = [
     'gpt-3.5-turbo',
-    ''
+    'gpt-4'
 ]
 
 
 async def get_scrap_completion(model: str, messages: list) -> str or None:
+    if not model in allowed_models:
+        return None
+
 
     response = await g4f.ChatCompletion.create_async(
         model=model,
         messages=messages,
-        provider=choice(providers),
+        provider=choice(providers[model]),
     )
     return response
